15781be80fa2341ea764b965433f0b610cdc2304
Adnan <adnanmuhammedadnancp@gmail.com>
2025-01-05 13:25:23 +0530

    add Dockerfile

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-# from app import create_app
-#
-# app = create_app()
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from app import create_app
 
+app = create_app()
 
-import pandas as pd
-from app.utils import get_prediction
-
-df = pd.read_csv("~/Downloads/aqi_input_template.csv")
-out = get_prediction(df)
-
-print(out)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
